fe6dcaa3be1edb4c18ce598048471ae735dfc3c8
Shantanu675 <shantanudanej05@gmail.com>
2025-07-20 22:46:06 +0530

    Spring Security Implementation

---
+++ src/main/java/com/security/SpringSecurityExample/Student.java
@@ -0,0 +1,47 @@
+package com.security.SpringSecurityExample;
+
+public class Student {
+
+    private String name;
+    private int age;
+    private double marks;
+
+    public Student(String name, int age, double marks) {
+        this.name = name;
+        this.age = age;
+        this.marks = marks;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getMarks() {
+        return marks;
+    }
+
+    public void setMarks(double marks) {
+        this.marks = marks;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", marks=" + marks +
+                '}';
+    }
+}
---
+++ src/main/java/com/security/SpringSecurityExample/StudentController.java
@@ -0,0 +1,36 @@
+package com.security.SpringSecurityExample;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class StudentController {
+
+    List<Student> students = new ArrayList<>(List.of(
+            new Student("shantanu", 18, 9.41),
+            new Student("shreyash", 14, 9.21)
+    ));
+
+    @GetMapping("/students")
+    public List<Student> getListOfStudents(){
+        return students;
+    }
+
+    @GetMapping("/csrf")
+    public CsrfToken getToken(HttpServletRequest request){
+        return (CsrfToken) request.getAttribute("_csrf");
+    }
+
+    @PostMapping("/students")
+    public void addStudent(@RequestBody Student student){
+        students.add(student);
+    }
+};
